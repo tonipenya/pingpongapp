1c77a1771a636653b91544e2dc8e9ccaf36774a4
lessbloat <pass99w>
2010-04-09 21:06:25 +1000

    the rest of my home page login tweaks

--- pingpong/media/core.js
+++ pingpong/media/core.js
@@ -102,6 +102,35 @@ function loginCallback(data) {
 		window.location = data;
 	}
 }
+function loginClear (which) {
+	if (which === "username") {
+		var loginUsername = $("#loginUsername").val();
+		if (loginUsername === "Username") {
+			$("#loginUsername").val("");
+		}
+	} else {
+		var loginPassword = $("#loginPassword").val();
+		if (loginPassword === "Password") {
+			$("#loginPassword").val("");
+		}
+	}
+}
+function loginFormValue () {
+	var loginUsername = $("#loginUsername").val();
+	var loginPassword = $("#loginPassword").val();
+	if (loginUsername === "" || loginUsername === "Username") {
+		$("#loginUsername").val("Username");
+		$("#loginUsername").css("color", "#999");
+	} else {
+		$("#loginUsername").css("color", "#333");
+	}
+	if (loginPassword === "" || loginPassword === "Password") {
+		$("#loginPassword").val("Password");
+		$("#loginPassword").css("color", "#999");
+	} else {
+		$("#loginPassword").css("color", "#333");
+	}
+}
 // LOADING /////////////////////////////
 function coreLoading (message)
 {
