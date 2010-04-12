55fd3da55409a40fea1ab6ad19aabb36537f4f1e
U-FRESHVIEW\jamesd <jamesd@jd.in.freshview>
2010-04-12 17:33:38 +1000

    Added more js to app media. Added favicon.

--- pingpong/settings.py
+++ pingpong/settings.py
@@ -2,6 +2,8 @@ from ragendja.settings_post import settings
 
 settings.add_app_media('combined-%(LANGUAGE_CODE)s.js',
     'pingpong/core.js',
+    'pingpong/init.js',
+    'pingpong/home.js',
 )
 settings.add_app_media('combined-%(LANGUAGE_DIR)s.css',
     'pingpong/core.css',
